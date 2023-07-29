874e29920d74143997f8a9d2af5babe07a4262cb
Your Name <metayerkinson@gmail.com>
2023-07-29 14:58:09 -0400

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
-
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+const cors = require("cors");
 
 const PORT = 3001;
 const app = express();
@@ -9,11 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
-
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-    });
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
+});
